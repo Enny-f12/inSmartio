87d9af02ca559b98c3b0194cf233b6652dd197f2
Alayande Esther <alayandeesther469@gmail.com>
2026-05-22 22:42:50 +0100

    added image patterns

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,19 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "img.com",
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
       },
     ],
   },
